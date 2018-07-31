65132fd5fde98222330b60ce6c71f2dab97338fa
Luis Quintanilla <lqdev@outlook.com>
2018-07-31 17:24:07 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ASP.NET Core in Action
+
+This repo contains content and exercises from the [ASP.NET Core in Action](https://www.manning.com/books/asp-net-core-in-action) book 
+
+## Chapter Structure
+- Chapter 2
+    - WebApplication2
+- Chapter 3
+    - Welcome Page
---
+++ WelcomePage/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WelcomePage
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
